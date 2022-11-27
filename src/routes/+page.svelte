6ba15dca49a6af4f532753b70579d2fd5456d5f6
shaOne macbook <web@sccreative.co.uk>
2022-11-27 11:05:40 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<div class="content-section">
+  <div class="content-section-inner text-center">
+    <h1 class="pb-0 text-amber-400 font-comfortaa tracking-tight font-semibold font text-5xl mb-3">Mae Bird</h1>
+    <p class= "text-xl text-orange-200 max-w-lg mx-auto"><strong class="text-amber-300">Singer/song-channeller</strong> singing for <span class="">healing and love</span>, <br /><span class="">inspired by nature.</span></p> 
+    <div class="profile-image-container mt-6">
+      <img class="profile-image" src="mae-profile-square.jpeg" alt="mae bird profile shot">
+    </div>
+    <div class="link-container mt-6">
+      <a class="link" target="_blank" href="https://www.instagram.com/maebirdsings/">Instagram</a>
+      <a class="link" target="_blank" href="https://www.facebook.com/maebirdsings">Facebook</a>
+      <a class="link" target="_blank" href="https://maebird.bandcamp.com/releases">Bandcamp</a>
+      <a class="link" target="_blank" href="https://soundcloud.com/mae-gomersall-870459893">Soundcloud</a>
+    </div>
+  </div>
+</div>
+<div class="content-section">
+  <div class="content-section-inner">
+  </div>
+</div>
+
+<style lang="postcss">
+  .link-container {
+    @apply flex flex-col;
+  }
+  .link {
+    @apply font-comfortaa tracking-tight text-center text-amber-300;
+    @apply bg-gray-800 p-4 mb-3 mx-auto w-full max-w-lg;
+  }
+  .link:hover {
+    @apply  text-amber-200 bg-gray-700;
+  }
+  .profile-image-container {
+    @apply max-w-xs mx-auto;
+  }
+  .profile-image {
+    @apply rounded-full;
+  }
+</style>
