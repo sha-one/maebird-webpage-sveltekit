4f4f5cc0147a7913dcadd66605406becdb0af30b
shaOne macbook <web@sccreative.co.uk>
2023-07-03 14:32:33 +0100

    macbook: gig updates

---
+++ src/routes/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+
+</script>
+
+<div class="content-section">
+  <div class="content-section-inner text-center">
+    <h1 class="pb-0 text-amber-400 font-comfortaa tracking-tight font-semibold font text-5xl mb-3">Mae Bird</h1>
+    <p class= "text-xl text-orange-200 max-w-lg mx-auto"><strong class="text-amber-300">Singer/song-channeller</strong> <span class="">singing for healing and love inspired by nature.</span></p> 
+    <div class="profile-image-container mt-6">
+      <img class="profile-image" src="mae-profile-square.jpeg" alt="mae bird profile shot">
+    </div>
+    <div class="gigs">
+      <h3 class="mt-12">Upcoming concerts July/August (UK)</h3>
+      <p><strong>Hola tribe!</strong></p>
+      <p>So I will be coming back to the UK at the end of July to play some concerts in some wonderful places I have curated with integrity with the hep of some very supportive people around England! </p>
+      <h4 class="mt-8">Dates confirmed:</h4>
+      <p>
+        <ul>
+          <li>29th July in Brighton</li>
+          <li>4th August in Dorsey (Our light gathering)</li>
+          <li>5th August in the New forest</li>
+          <li>11th August in Sowerby Bridge</li>
+          <li>13th August in Leeds</li>
+          <li>25th August at The Village festival, midlands</li>
+
+        </ul>
+        <p>
+      <p class="mt-8 text-sm mb-4">
+        More details to come, if you would like more info about one of these events send me a message and I will respond with love!
+        Thank you the wonderful spaces being created and all the people involved in these gatherings. 
+      </p>
+      <p class=" text-gray-400"><i>#summer #livemusic #heartsongs #gratidão</i></p>
+        
+   
+    </div>
+    <div class="link-container mt-6">
+      <a class="link" target="_blank" href="https://maebird.bandcamp.com/releases">Bandcamp</a>
+      <a class="link" target="_blank" href="https://soundcloud.com/mae-gomersall-870459893">Soundcloud</a>
+      <a class="link" target="_blank" href="https://open.spotify.com/artist/7dEdtwQRLFre23Rm62OQvR?si=VM5v_sFPRdSPFBWafxMnkg&nd=1">Spotify</a>
+      <a class="link" target="_blank" href="https://www.instagram.com/maebirdsings/">Instagram</a>
+      <a class="link" target="_blank" href="https://www.facebook.com/maebirdsings">Facebook</a>
+    </div>
+  </div>
+</div>
+<div class="content-section">
+  <div class="content-section-inner">
+  </div>
+</div>
+
+<style lang="postcss">
+  .link-container {
+    @apply flex flex-col;
+  }
+  .link {
+    @apply font-comfortaa tracking-tight text-center text-amber-300;
+    @apply bg-gray-800 p-4 mb-3 mx-auto w-full max-w-lg;
+  }
+  .link:hover {
+    @apply  text-amber-200 bg-gray-700;
+  }
+  .profile-image-container {
+    @apply max-w-xs mx-auto;
+  }
+  .profile-image {
+    @apply rounded-full;
+  }
+  h3, h4 {
+    @apply font-semibold text-amber-300;
+  }
+  .gigs {
+    @apply max-w-lg mx-auto;
+  }
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        'comfortaa': ['"Comfortaa"', 'cursive'],
+      },
+    },
+  },
+
+  plugins: [],
+};
+
+module.exports = config;
